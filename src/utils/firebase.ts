7b7c9aaf8128e9dbbab34e3a0170ff927dd7eec0
Metehan Anlı <metehananli@gmail.com>
2026-06-17 17:11:06 +0300

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwMBYD1jZSi-vEhKKA1k_fLeLae6foniU",
-  authDomain: "borsa-defterim.firebaseapp.com",
-  projectId: "borsa-defterim",
-  storageBucket: "borsa-defterim.firebasestorage.app",
-  messagingSenderId: "858014537516",
-  appId: "1:858014537516:web:8b5562fee6503cb5e9be89"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
